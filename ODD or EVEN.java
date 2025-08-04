2be26fd58dde354873179060d99125655bff6326
Prajeen-2 <prajeenhunte@gmail.com>
2025-08-04 15:51:08 +0530

    Create ODD or EVEN.java

---
+++ ODD or EVEN.java
@@ -0,0 +1,20 @@
+     import java.util.Scanner;
+
+class Main{
+    public static void main(String[]args) {
+        Scanner data = new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int num = data.nextInt();
+        if (num%2==0){
+            System.out.println("even");
+        
+        }
+        else{
+           System.out.println("odd");
+       }
+        
+        
+        
+        
+    }
+}
