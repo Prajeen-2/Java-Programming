06f4bfec4108a3028467a1b186d8d29818841887
Prajeen-2 <prajeenhunte@gmail.com>
2025-08-05 11:46:14 +0530

    Create count.java

---
+++ count.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        int i =10;
+        Scanner data = new Scanner(System.in);
+        for(int count = 1; count<=10; count++)
+        {
+            System.out.println(count);
+        }
+    }
+}
+        
