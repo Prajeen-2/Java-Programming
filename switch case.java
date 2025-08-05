aa7144b189ef07d101b54887ab003b933ab4e7e3
Prajeen-2 <prajeenhunte@gmail.com>
2025-08-05 09:56:23 +0530

    Create switch case.java

---
+++ switch case.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+class main {
+
+    public static void main(String[] args) {
+
+        Scanner data = new Scanner(System.in);
+        
+        int day;
+        System.out.print("Enter your number:");
+        day=data.nextInt();
+        
+        switch(day){
+            
+            case 1:
+                
+                System.out.println("sunday");
+                break;
+                
+            case 2:
+                
+                System.out.println("monday");
+                break;
+            case 3:
+                
+                System.out.println("tuesday");
+                break;
+                
+            case 4:
+                
+                System.out.println("wednesday");
+                break;
+            case 5:
+                
+                System.out.println("thursday");
+                break;
+            case 6:
+                
+                System.out.println("friday");
+                break;
+            case 7:
+                
+                System.out.println("saturday");
+                break;
+            
+        }
+    }
+}
+
+            
+            
+            
+            
+            
+            
+                
+            
+            
+            
+        
