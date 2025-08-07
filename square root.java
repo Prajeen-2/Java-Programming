ca33e7557091bb618daba8cbfe5a251360a0fe47
Prajeen Kumar <prajeenhunte@gmail.com>
2025-08-07 10:18:54 +0530

    Create square root.java

---
+++ square root.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[]args){
+        Scanner data= new Scanner(System.in);
+        int num;
+        System.out.println("Enter a number");
+        num=data.nextInt();
+        System.out.println("Square Root = " + (num*num));}
+}
+        
+        
+        
