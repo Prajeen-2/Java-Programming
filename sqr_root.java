af81b4f30da76f4c86f30f7e4e90a9fe5080c7a3
Prajeen Kumar <prajeenhunte@gmail.com>
2025-08-07 11:11:28 +0530

    Create sqr_root.java

---
+++ sqr_root.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[]args){
+        Scanner data= new Scanner(System.in);
+        int num;
+        System.out.println("Enter a number");
+        num=data.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int i=1;i<=n;j++){
+        System.out.println("Square Root = " + (num*num));
+        num++;
+            }
+        }
+    }
+    System.out.print();
+
+
+}
+        
+        
+        
