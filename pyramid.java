185d3a16df24c5be7e6fdbfc0b0192ed89b5a018
Prajeen Kumar <prajeenhunte@gmail.com>
2025-08-07 12:36:14 +0530

    Create pyramid.java

---
+++ pyramid.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner data=new Scanner(System.in);
+	 int rows;
+        rows=data.nextInt();
+        System.out.println("Enter a element");
+        
+
+        for (int i = 1; i <= rows; i++) {
+            
+            for (int j = i; j < rows; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+                
+            }
+            System.out.println();
+        }
+	}
+}
