f17361731c4294c8f89f90e7ca6c9cc85535cb2b
Prajeen Kumar <prajeenhunte@gmail.com>
2025-08-08 14:34:14 +0530

    Create addition with 2 classes.java

---
+++ addition with 2 classes.java
@@ -0,0 +1,20 @@
+class Sample {
+    
+	public void add() {
+	    
+		int x=485;
+		
+		int y=305;
+		
+		System.out.println(x * y);
+	}
+}
+class Main {
+    
+	public static void main(String[]args) {
+	    
+		Sample obj = new Sample();
+		
+		obj.add();
+	}
+}
