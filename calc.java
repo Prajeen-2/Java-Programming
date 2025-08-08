676aa3606e5aab9269ea9269489e948edfb757f6
Prajeen Kumar <prajeenhunte@gmail.com>
2025-08-08 12:02:18 +0530

    Create calc.java

---
+++ calc.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+class Main{
+    public static void main(string[]args){
+        public static int add(int a, int b){
+            return a+b;}
+        
+        public static int mul(int a,int b){
+            return a-b;}
+        
+        public static int div(int a,int b){
+            return a/b;}
+            
+        public static int mod(int a,int b){
+            return a%b;}
+
+        
+        public static int mul(int a,int b){
+            return a*b;}
+        }
+        
+        
+    Scanner.data=new Scanner(System.in);
+    int x;
+    int x=data.nextInt();
+    System.out.println("Enter an number");
+    int y;
+    int y=data.nextInt();
+    System.out.println("Enter an operation");
+    System.out.println("1.add");
+    System.out.println("2.sub");
+    System.out.println("3.div");
+    System.out.println("4.mod");
+    System.out.println("5.mul");
+    System.out.println("Enter an number");
+    Scanner x = new Scanner(System.in);
+    int opr = x.nextInt();
+     double result = 0;
+        switch (choice) {
+            case 1:
+                result = add(num1, num2);
+                break;
+            case 2:
+                result = sub(num1, num2);
+                break;
+                
+            case 3:
+                result = div(num1, num2);
+                break;
+                
+            case 4:
+                result = mod(num1, num2);
+                break;
+                
+            case 5:
+                result = mul(num1,num2);
+                break;
+                
+            System.out.println("The Answer is: " + );
+        
+        }
+            
+            
+                
+    
+    }
+
+}
+    
+    
+    
+    
