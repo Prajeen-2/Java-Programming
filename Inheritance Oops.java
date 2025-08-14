ac83bf0fbb4feaf7a266c8bf7aee326ba4f179f4
Prajeen Kumar <prajeenhunte@gmail.com>
2025-08-14 09:42:39 +0530

    Inheritance Oops.java

---
+++ Inheritance Oops.java
@@ -0,0 +1,43 @@
+class Cars{
+    void sound(){
+        System.out.println("Cars makes a sound");
+        
+    }
+}
+
+class BMW extends Cars {
+    void sound(){
+        System.out.println("BMW");
+        
+    }
+}
+
+class Audi extends Cars{
+    void sound(){
+        System.out.println("Audi");
+        
+    }
+}
+
+class Benz extends Cars{
+    void sound(){
+        System.out.println("Benz");
+        
+    }
+}
+
+
+public class Main{
+    public static void main(String[]args){
+        Cars a;
+        a = new BMW();
+        a.sound();
+        
+        a = new Audi();
+        a.sound();
+        
+        a = new Benz();
+        a.sound();
+        
+    }
+}
